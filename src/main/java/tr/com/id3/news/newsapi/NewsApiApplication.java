4eae6a0f18fa7ac64a1896dcfb029639dc984703
Cengizhan Şahin <cengizhan.sahin@id3.com.tr>
2022-08-02 15:32:06 +0300

    NWSP-11 Initial Commit Project Setup

---
+++ src/main/java/tr/com/id3/news/newsapi/NewsApiApplication.java
@@ -0,0 +1,13 @@
+package tr.com.id3.news.newsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApiApplication.class, args);
+	}
+
+}
